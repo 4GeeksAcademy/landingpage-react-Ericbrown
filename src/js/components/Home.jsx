59fdc3318958b546366f46bdfdfc3531ed235ff3
eric499 <100245477+eric499@users.noreply.github.com>
2025-07-23 03:22:13 +0000

    proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./CardComponents";
+import Footer from "./Footer";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const tours = [
+        {
+            image: "https://powergpu.com/wp-content/uploads/2023/05/DSC_7348-thumb-scaled.jpg",
+            title: "Intel Core 9 , Aorus RTX 5090 ...",
+            description: "Intel Ultimate Pc"
+        },
+        {
+            image: "https://powergpu.com/wp-content/uploads/2025/05/DSC_9512-768x432.jpg",
+            title: "AMD Ryzen 9 9950X3D, MSI RTX 5090",
+            description: "AMD Ultimate Pc"
+        },
+        {
+            image: "https://dcdn-us.mitiendanube.com/stores/004/992/029/products/xbox-series-x-05b38518d22ac00b6317219456885009-1024-1024.jpg",
+            title: "XBOX Series X",
+            description: "Console"
+        },
+        {
+            image: "https://m.media-amazon.com/images/I/51fM0CKG+HL.jpg",
+            title: "PlayStation 5",
+            description: "Console"
+        }
+    ];
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <Navbar />
+            <Jumbotron />
+            <div className="container my-4 flex-grow-1">
+                <h2 className="text-center mb-4 fw-bold">Products</h2>
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+                    {tours.map((tour, index) => (
+                        <div className="col" key={index}>
+                            <Card {...tour} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
 };
-
 export default Home;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron bg-black bg-gradient py-5 mb-4 text-white">
+            <div className="container text-center">
+                <h1 className="display-5 fw-bold">Custom Pc & More...</h1>
+                <p className="lead opacity-75"></p>
+                <button className="btn btn-light btn-lg mt-3 px-4 fw-semibold">
+                    Promociones
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
